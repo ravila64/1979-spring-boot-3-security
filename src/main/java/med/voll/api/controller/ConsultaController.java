8f08ee2c432f223b798baaafe3d73bbac1d87fc3
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2023-09-27 19:56:57 -0500

    hasta test en repository

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -5,27 +5,28 @@ import jakarta.validation.Valid;
 
 import med.voll.api.domain.consulta.AgendaDeConsultaService;
 import med.voll.api.domain.consulta.DatosAgendarConsulta;
+import med.voll.api.domain.consulta.DatosDetalleConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
 @ResponseBody
 @RequestMapping("/consultas")
 @SecurityRequirement(name = "bearer-key")
-//@SuppressWarnings("all")
+@SuppressWarnings("all")
 public class ConsultaController {
-
     @Autowired
-    private AgendaDeConsultaService agendaDeConsultaService;   // estaba service
-
+    private AgendaDeConsultaService service;
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos){
-        var response = agendaDeConsultaService.agendar(datos);
+        var response = service.agendar(datos);
         return ResponseEntity.ok(response);
-        // ret ResponseEntity.ok(new DatosDetalleConsulta(null,null,null,null));
+//        return ResponseEntity.ok(new DatosDetalleConsulta(response.id(),response.idPaciente(),response.idMedico(),
+//                            response.fecha()));
     }
 }
--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -1,19 +1,99 @@
 package med.voll.api.domain.medico;
 
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.direccion.DatosDireccion;
+import med.voll.api.domain.direccion.Direccion;
+import med.voll.api.domain.paciente.Paciente;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjuster;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
-// se utiliza una base de datos externa
+// se utiliza una base de datos externa, y se quita linea si es basedata in memory
+// y activeProfile()
 @ActiveProfiles("test")
 class MedicoRepositoryTest {
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @Autowired
+    private TestEntityManager em;
 
     @Test
-    void seleccionarMedicoConEspecialidadEnFecha() {
+    @DisplayName("Deberia Retornar Nulo cuando el medico se encuebtre en consulta con paciente en esa hora")
+    void seleccionarMedicoConEspecialidadEnFechaEscenario1() {
+        var proximoLunes10H = LocalDate.now()
+                .with(TemporalAdjuster.next(DayOfWeek.MONDAY)).atTime(10,0);
+
+        var medico=registrarMedico("Jose","j@mail.com","123456",Especialidad.CARDIOLOGIA);
+        var paciente=registrarPaciente("Antonio","a@mail.com","645213");
+        registrarConsulta(medico,paciente,proximoLunes10H);
+
+        var medicoLibre = medicoRepository.seleccionarMedicoConEspecialidadEnFecha(Especialidad.CARDIOLOGIA, proximoLunes10H);
+
+        assertThat(medicoLibre).isNull();
     }
+
+    private void registrarConsulta(Medico medico, Paciente paciente, LocalDateTime fecha){
+        em.persist(new Consulta(null, medico, paciente, fecha, null));
+    }
+    private  Medico registrarMedico (String nombre, String email, String telefono, String dni, Especialidad especialidad ){
+        var medico = new Medico(new datosMedico(nombre, email, telefono, dni, especialidad));
+        em.persist(medico);
+        return  medico;
+    }
+    private Paciente registrarPaciente (String nombre, String email, String documento){
+        var paciente= new Paciente(new DatosPaciente(nombre, email, documento));
+        em.persist(paciente);
+        return  paciente;
+    }
+
+    private DatosRegistroMedico datosMedico(String nombre, String email, String telefono, String dni,
+                                            Especialidad especialidad, Direccion direccion){
+        return new DatosRegistroMedico(
+                nombre,
+                email,
+                telefono,
+                dni,
+                especialidad,
+                datosDireccion()
+        );
+    }
+
+    private DatosDireccion datosDireccion(){
+        return new DatosDireccion("loca",
+                "azul",
+                "Acapulco",
+                "321",
+                "12"
+        );
+    }
+
+    private record datosMedico(
+            @NotNull String nombre,
+            @NotNull String email,
+            @NotNull String dni,
+            @NotNull Especialidad especialidad) {
+    }
+
+    private record DatosPaciente(
+            @NotNull String nombre,
+            @NotNull String email,
+            @NotNull String documento){
+    }
+
+//    void seleccionarMedicoConEspecialidadEnFecha() {
+//    }
 }
