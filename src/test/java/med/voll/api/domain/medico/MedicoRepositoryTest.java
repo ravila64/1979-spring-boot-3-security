0af4edb5919273b728271fdd427b9f8daf0772f2
Rene Avila <rene.avila.alonso@gmail.com>
2023-09-28 07:50:20 -0500

    arreglo TemporalAdjusters

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -16,7 +16,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -35,9 +35,8 @@ class MedicoRepositoryTest {
     @DisplayName("Deberia Retornar Nulo cuando el medico se encuebtre en consulta con paciente en esa hora")
     void seleccionarMedicoConEspecialidadEnFechaEscenario1() {
         var proximoLunes10H = LocalDate.now()
-                .with(TemporalAdjuster.next(DayOfWeek.MONDAY)).atTime(10,0);
-
-        var medico=registrarMedico("Jose","j@mail.com","123456",Especialidad.CARDIOLOGIA);
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10,0);
+        var medico=registrarMedico("Jose","j@mail.com","123456","654321",Especialidad.CARDIOLOGIA);
         var paciente=registrarPaciente("Antonio","a@mail.com","645213");
         registrarConsulta(medico,paciente,proximoLunes10H);
 
@@ -49,8 +48,8 @@ class MedicoRepositoryTest {
     private void registrarConsulta(Medico medico, Paciente paciente, LocalDateTime fecha){
         em.persist(new Consulta(null, medico, paciente, fecha, null));
     }
-    private  Medico registrarMedico (String nombre, String email, String telefono, String dni, Especialidad especialidad ){
-        var medico = new Medico(new datosMedico(nombre, email, telefono, dni, especialidad));
+    private  Medico registrarMedico (String nombre, String email, String telefono, String dni, Especialidad especialidad, Direccion direccion ){
+        var medico = new Medico(new datosMedico(nombre, email, telefono, dni, especialidad, new Direccion(datosDireccion())));
         em.persist(medico);
         return  medico;
     }
@@ -60,20 +59,21 @@ class MedicoRepositoryTest {
         return  paciente;
     }
 
-    private DatosRegistroMedico datosMedico(String nombre, String email, String telefono, String dni,
-                                            Especialidad especialidad, Direccion direccion){
+    private DatosRegistroMedico datosMedico(String nombre, String email, String  telefono, String dni,
+                                            Especialidad especialidad,Direccion direccion){
         return new DatosRegistroMedico(
                 nombre,
                 email,
                 telefono,
                 dni,
                 especialidad,
-                datosDireccion()
+                DatosDireccion()
         );
     }
 
     private DatosDireccion datosDireccion(){
-        return new DatosDireccion("loca",
+        return new DatosDireccion(
+                "loca",
                 "azul",
                 "Acapulco",
                 "321",
@@ -84,8 +84,10 @@ class MedicoRepositoryTest {
     private record datosMedico(
             @NotNull String nombre,
             @NotNull String email,
+            @NotNull String telefono,
             @NotNull String dni,
-            @NotNull Especialidad especialidad) {
+            @NotNull Especialidad especialidad,
+            @NotNull Direccion direccion) {
     }
 
     private record DatosPaciente(
